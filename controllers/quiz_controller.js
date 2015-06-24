3332c2c23a0ab66ab7f7182d55b6aacd96be8074
maRc2 <mamigove3@yahoo.es>
2015-06-24 22:02:23 +0200

    Rama agregados creditos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -10,3 +10,8 @@ exports.answer = function(req, res) {
 	else
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 };
+
+// GET /author
+exports.author = function(req, res) {
+	res.render('author', {title: 'Quiz', author: 'maRc', desc_author: 'Desarrollador Node'});
+};
